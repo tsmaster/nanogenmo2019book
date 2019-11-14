ed9272e0ac456911a7733864f7bf5034bdca6885
Dave LeCompte <tsmaster@gmail.com>
2019-11-14 08:24:22 -0800

    Added word wrap

---
+++ Scripts/wordwrap.py
@@ -0,0 +1,39 @@
+def wordwrap(s, wrap_col):
+    paragraphs = s.split('\n')
+    for pi, p in enumerate(paragraphs):
+        paragraphs[pi] = wrapParagraph(p, wrap_col)
+
+    return "\n".join(paragraphs)
+
+def wrapParagraph(p, wrap_col):
+    if not p:
+        return ''
+
+    if len(p) == 0:
+        return ''
+    if len(p) < wrap_col:
+        return p
+    
+    first_line = p[:wrap_col]
+    breakIndex = first_line.rindex(' ')
+
+    if breakIndex == -1:
+        rest = p[wrap_col:]
+        return first_line + '\n' + wrapParagraph(rest, wrap_col)
+    else:
+        first = first_line[:breakIndex]
+        rest = p[breakIndex + 1:]
+        return first + '\n' + wrapParagraph(rest, wrap_col)
+
+
+def test():
+    with open("book.txt", "rt") as f:
+        text = f.read()
+        print wrapParagraph(text, 65)
+
+if __name__ == "__main__":
+    test()
+    
+
+        
+    
